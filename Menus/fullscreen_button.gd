80f365f2c2537444a1322087efed822660a6376f
AnAirBreather <Aocowell@icloud.com>
2026-03-22 18:31:16 +0000

    Small changes

--- Menus/fullscreen_button.gd
+++ Menus/fullscreen_button.gd
@@ -2,7 +2,6 @@ extends CheckButton
 
 
 
-
 func _on_toggled(toggled_on: bool) -> void:
 	if toggled_on == true: #If the switch is pressed
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN) #Change to fullscreen modee
