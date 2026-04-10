a722421e76ddb42e778e73ee2607a11e6587a377
AnAirBreather <Aocowell@icloud.com>
2026-04-10 17:38:11 +0100

    Fullscreen toggle fix

--- Menus/fullscreen_button.gd
+++ Menus/fullscreen_button.gd
@@ -1,9 +1,11 @@
 extends CheckButton
 
-
+func _ready() -> void:
+	if (DisplayServer.window_get_mode() == 3):
+		button_pressed = true
 
 func _on_toggled(toggled_on: bool) -> void:
 	if toggled_on == true: #If the switch is pressed
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN) #Change to fullscreen modee
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN) #Change to fullscreen mode
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED) #Change to windowed mode
